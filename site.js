18b819c28575cf61c598e21e5fd606c915490591
mariaapapadimitriou <maria.papadimitriou@mail.utoronto.ca>
2022-01-31 11:05:01 -0500

    bug fixes

--- site.js
+++ site.js
@@ -10,21 +10,23 @@ form.addEventListener('submit', function(e) {
     loader.style.display = 'flex';
       
     document.getElementById("container").innerHTML = "";
-    const start_date = document.getElementById("startdate").value
-    const end_date = document.getElementById("enddate").value
+    var start_date = document.getElementById("startdate").value
+    var end_date = document.getElementById("enddate").value
     
-    const url = 'https://api.nasa.gov/planetary/apod?api_key=0pGY5v8sAphcSRyVocADwQBcFh6jaaP2WHa8dkMM&start_date=' + start_date + '&end_date=' + end_date
+    const url = 'https://api.nasa.gov/planetary/apod?api_key=0pGY5v8sAphcSRyVocADwQBcFh6jaaP2WHa8dkMM&start_date=' + start_date + "&end_date=" + end_date
+
+    let requestURL = url;
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
     
-    try {
-      const response = await fetch(`${url}`)
-      const data = await response.json()
-      console.log('NASA APOD data', data)
+    request.onload = function() {
+      const data = request.response;
       displayData(data)
-    } catch (error) {
-      console.log(error)
+      loader.style.opacity = 0;
+      loader.style.display = 'none';
     }
-    loader.style.opacity = 0;
-    loader.style.display = 'none';
   }
 
   const displayData = data => {
@@ -35,7 +37,6 @@ form.addEventListener('submit', function(e) {
             continue;
         }
         if (localStorage.getItem(data[i].hdurl)) {
-            console.log("Hi")
             if (localStorage.getItem(data[i].hdurl) == 0) { // IF PHOTO IN LIKED PHOTOS AND NOT LIKED
                 dynamic_images += "<div class='picture-explanation-container'><p style='font-weight: 900; font-size: 20px;'><span>"+ data[i].title +"</span><span style='font-weight: lighter; font-size: 13px; color: #353535'>"+ data[i].date +"</span></p><img class='posts' src='" + data[i].hdurl + "' height='100%' width='100%'/><p style='justify-content: start;'><button class='likebutton' id='" + data[i].hdurl + "' onclick='liked(this.id)'><i class='far fa-heart fa-3x'></button></i></p><p style='justify-content: flex-start; font-weight: bold;'>Description</p><p>" + data[i].explanation + "</p></div>"
             }
@@ -53,8 +54,6 @@ form.addEventListener('submit', function(e) {
 
   function liked(btn) {
       var x = localStorage.getItem(btn);
-
-    console.log(x)
 
     if (localStorage.getItem(btn) == 0) { //IF NOT LIKED
         document.getElementById(btn).innerHTML = "<i class='fas fa-heart fa-3x'></i>"
